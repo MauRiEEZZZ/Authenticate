89b82911f58a37a35527484902164323f803dd7c
Meauris <meauris@outlook.com>
2019-10-06 23:03:50 +0200

    updated connectionstring module

--- Helpers/connectionstr.py
+++ Helpers/connectionstr.py
@@ -3,6 +3,6 @@ def loganalytics(tenant, clientid, secret, workspaceid):
     Concatenate strings into the full loganalytics connection string
     """
 
-    result = 'loganalytics://tenant=\''+tenant+'\';clientid=\''+clientid+'\';clientsecret=\''+secret+'\';workspace=\''+workspaceid+'\';'
+    result = '\'loganalytics://tenant=\''+tenant+'\';clientid=\''+clientid+'\';clientsecret=\''+secret+'\';workspace=\''+workspaceid+'\';\''
 
     return result
