23b85b8712ac9233c1e1b75dfb0e6b9aefcf8c63
Maurice MJ de Jong <maurieezzz@hotmail.com>
2020-06-18 11:28:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
    version='0.1.11',
    description='this module will help you authenticate to azure resources used in your python script',
    author='Maurice de Jong',
-   author_email='maurice@deholanda.com',
+   author_email='maurieezzz@hotmail.com',
    packages=['Helpers'],  #same as name
    install_requires=['adal', 'azure'], #external packages as dependencies
 )
